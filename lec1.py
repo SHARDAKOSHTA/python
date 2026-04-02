77b0445f66290bee1f92b2c425d5d4349e0d0b26
SHARDAKOSHTA <shardakoshta415@gmail.com>
2026-04-02 13:34:24 +0530

    python code

---
+++ lec1.py
@@ -0,0 +1,15 @@
+print("sharda koshta")
+x=3;
+y=5;
+z=3;
+print(id(x),id(y),id(z))
+y=8;
+print(id(x),id(y),id(z))
+name=input("enter name")
+print(name)
+age=int(input("enter age"))
+print(age)
+
+height=input("enter height")
+print(height)
+print("my name",name,"age",age,"height",height)
