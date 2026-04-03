da5d31311b6fda668f71e07d198d1a5fe7d12730
SHARDAKOSHTA <shardakoshta415@gmail.com>
2026-04-03 09:46:58 +0530

    lec.py

---
+++ lec2.py
@@ -0,0 +1,68 @@
+
+
+
+# width=float(input("enter width: "))
+# print(width)
+# length=float(input("enter length : "))
+# print(length)
+# area=length*width
+# print("area of a rec",area)
+
+
+# radius=float(input("enter radius : "))
+# print(radius)
+# circumfrence=2*3.14*radius
+# print("circum of circle : ",circumfrence)
+
+
+
+# age=int(input("Enter age : "))
+# print(age)
+# if age>=18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible for vote")
+
+
+# num=int(input("enter num : "))
+# if num>0:
+#     print("num is positive")
+# elif num<0:
+#     print("num is negative")
+# else:
+#     print("num is zero")
+
+
+# num=int(input("enter num : "))
+# if num%2==0:
+#         print("num is even")
+# else:
+#         print("num is odd")
+
+
+# year=int(input("enter num : "))
+# if (year%4==0 and year%100!=0) or(year%400==0):
+#         print("leap year")
+# else:
+#         print("not leap year")
+
+
+# num=int(input("enter num : "))
+# if num%3==0 and num%5==0:
+#  print("num is divisible by both")
+# else:
+#   print("num is not divisible by both")
+
+
+# month=int(input("enter month : "))
+# if month>=3 and month<=5:
+#     print("spring")
+# elif month>=6 and month<=8:
+#     print("summer")
+# elif month>=9 and month<=11:
+#     print("autumn")
+# else:
+#     print("winter")
+
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+radius=float(input("enter radius : "))
+print(radius)
+circumfrence=2*3.14*radius
+print("circum of circle : ",circumfrence)
