e4cc7446f400fdd4e525b6c07ee5fb0bf8aaab05
SHARDAKOSHTA <shardakoshta415@gmail.com>
2026-04-27 15:16:59 +0530

    tuple.py

---
+++ tuple.py
@@ -0,0 +1,30 @@
+# tpl=()
+# tpl1=tuple()
+# print(tpl,tpl1)
+
+# tpl=1,2,"hello"
+# tpl[2]=9
+# del tpl
+# print(tpl)
+# print(type(tpl))
+
+# tpl=(1,2,3,4)
+# print(id(tpl))
+# tpl+=("world",2,8,9)
+# print(tpl)
+
+
+# tpl=(1,2,3,4)
+# tpl=list(tpl)
+# tpl[3]=9
+# tpl=tuple(tpl)
+# print(tpl)
+
+# tpl=(1,2,3,3,4,5,3)
+# print(sorted(tpl,reverse=False))
+# print(sorted(tpl,reverse=True))
+
+
+tpl=(77,7,7,6,4,3,6,7,7,77,1,2)
+tpl[-3: :-6]
+
