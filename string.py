39426caa37f035047edafa481a473b27260312c8
SHARDAKOSHTA <shardakoshta415@gmail.com>
2026-04-27 15:17:39 +0530

    string.py

---
+++ string.py
@@ -0,0 +1,15 @@
+# mystring="pythom Tu'torials"
+# print(mystring[-3:-8:-1])
+# print(mystring[-2:-4])
+
+# s1="hello"
+# s2="world"
+# s1=s1+s2
+# print(s1)
+
+# s=s1*4
+# print(s)
+
+x="Hello"
+x.lower()
+print(x)
