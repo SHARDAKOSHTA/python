c86330d97a8678fbc0e354466148bfb1fe8dae1b
SHARDAKOSHTA <shardakoshta415@gmail.com>
2026-05-25 12:35:15 +0530

    code

---
+++ assign.py
@@ -0,0 +1,48 @@
+# print int idx of char m
+
+# x="programming"
+# y="m"
+# for i in range (len(x)):
+#     if x[i]==y:
+#         print(i)
+
+# rev string
+# x="hello everyone lets learn python"
+# x=x.split()[::-1]
+# x=" ".join(x)
+
+# print(x)
+
+
+# x="hello everyone lets learn python"
+# print(" ".join(x.split()[::-1]))
+
+
+# x="python learn lets everyone hello"
+# rev=" ".join(reversed(x))
+# print(rev)
+
+
+
+# check if a given string is a valid or  not 
+
+# x="example@domain.com"
+# for i in range(len(x)):
+#     if 
+
+
+# remove test
+# x="This is a test string for testing"
+# x=x.replace("test","")
+# print(x)
+
+
+# x="programming"
+# seen=set()
+# for ch in x:
+#     if ch  not in seen:
+#         print(ch)
+#         seen.add(ch)
+    
+
+
